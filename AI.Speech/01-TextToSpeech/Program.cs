5d30bb464f419ba1f7f0ffc8a054fa135b202074
lan <iorilan@hotmail.com>
2018-09-16 22:05:48 +0800

    w

---
+++ AI.Speech/01-TextToSpeech/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TextToSpeech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = -3;     // -10...10
+
+            // Synchronous
+            synthesizer.Speak("Hello , Microsoft");
+
+            // Asynchronous
+            //synthesizer.SpeakAsync("Hello World");
+
+
+            Console.ReadLine();
+        }
+    }
+}
